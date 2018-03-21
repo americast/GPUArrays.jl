7f4c05e2a7cd286cf45859372fb79266ae79f06f
americast <sayan.sinha@iitkgp.ac.in>
2018-03-21 15:45:03 +0530

    pool

---
+++ src/pool.jl
@@ -0,0 +1,85 @@
+# function convolution_kernel(
+#         state,
+#         imgSrc::AbstractArray{T},
+#         kernelValues,
+#         kernel_width,
+#         imgConvolved,
+#         ::Val{BLOCK_SIZE},
+#         ::Val{LOCAL_WIDTH}
+#     ) where {T, BLOCK_SIZE, LOCAL_WIDTH}
+#     ui1 = UInt32(1); ui0 = UInt32(0)
+#     w = kernel_width
+#     wBy2 = w >> ui1 #w divided by 2
+#     #Goes up to 15x15 filters
+#     ptr = LocalMemory(state, T, LOCAL_WIDTH) # local width need to be static, so calculating it from block size won't cut it
+#     P = CLArrays.LocalArray{T, 2}(ptr, (LOCAL_WIDTH, LOCAL_WIDTH))
+#
+#     i = blockidx_x(state)
+#     j = blockidx_y(state) #Identification of work-item
+#     idX = threadidx_x(state)
+#     idY = threadidx_y(state)
+#
+#     ii = i*BLOCK_SIZE + idX; # == get_global_id(0);
+#     jj = j*BLOCK_SIZE + idY; # == get_global_id(1);
+#     #Reads pixels
+#     P[idX, idY] = imgSrc[ii, jj]
+#     #Needs to read extra elements for the filter in the borders
+#     if (idX < w)
+#         P[idX + BLOCK_SIZE, idY] = imgSrc[ii + BLOCK_SIZE, jj]
+#     end
+#     if (idY < w)
+#         P[idX, idY + BLOCK_SIZE] = imgSrc[ii, jj + BLOCK_SIZE]
+#     end
+#     synchronize_threads(state)
+#     ##############
+#     convPix = zero(T);
+#     for ix = ui0:(w - ui1)
+#         for jy = ui0:(w - ui1)
+#             temp = P[ix, jy]
+#             convPix += temp * kernelValues[ix + w*jy]
+#         end
+#     end
+#     ##############
+#     synchronize_threads(state)
+#     imgConvolved[ii + wBy2, jj + wBy2] = P[idX + wBy2, idY + wBy2]
+#     return
+# end
+
+
+function maxpool2d_kernel(state, A::AbstractArray{T}, out, Asize, pool, stride) where T
+    ilin = linear_index(state)
+    idx = GPUArrays.gpu_ind2sub(Asize, ilin)
+    if (idx[1] >= Asize[1] - Ksize[1] || idx[2] >= Asize[2] - Ksize[2])
+        return
+    end
+    temp_max = input[idx[1] + Asize[1] * idx[2]]
+    max_pos = idx[1] + Asize[1] * idx[2]
+
+    for p in 1:pool
+        m = input[idx[1] + stride * p + Asize[1] * idx[2]]
+        if (m > max)
+                temp_max = m
+                max_pos = idx[1] + stride * p + Asize[1] * idx[2]
+        end
+        out[idx[1] + size(out, 1) * idx[2]] = temp_max
+    end
+    return
+end
+
+
+function maxpool2d!(a, out, pool, stride)
+    gpu_call(maxpool2d_kernel, a, (a, out, UInt32.(size(a)), pool, stride))
+    GPUArrays.synchronize(out)
+    out
+end
+
+struct FFTKernel{T}
+    kernel::T
+    irfftplan
+    rfftplan
+end
+
+function fftkernel(A, kernel)
+    plan_rfft!(A)
+
+end
