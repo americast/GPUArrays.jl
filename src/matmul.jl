55100c5cab0c6bf7dd16041efdfd67842a328ee0
americast <sayan.sinha@iitkgp.ac.in>
2018-05-30 08:52:26 +0530

    MWE!

--- src/matmul.jl
+++ src/matmul.jl
@@ -1,32 +1,17 @@
+using CUDAnative
+
 function matmul_kernel(state, A::AbstractArray{T}, B::AbstractArray{T}, out, Asize, Bsize, outSize, ::Val{TS}, ::Val{TS²}, ::Val{IntTS²}) where {T, TS, TS², IntTS²}
-    # Thread identifiers
-    row = threadidx_x(state) # Local row ID (max: TS)
-    col = threadidx_y(state)
-    # col = row
-    # Local memory to fit a tile of TS*TS elements of A and B
-    Asub = @LocalMemory(state, T, TS²)
-    Bsub = @LocalMemory(state, T, TS²)
-
-    # Initialise the accumulation register
+    row = CUDAnative.threadIdx().x
+    Asub = CUDAnative.@cuStaticSharedMem(T, TS²)
     acc = T(0.0)
-
-    # Loop over all tiles
-    numTiles = div(Asize[2], TS)
-    for t in UInt32(1):UInt32(numTiles)
-        # Perform the computation for a single tile
-        # Commenting this inner loop doesn't give an error
+    for t in UInt32(1):UInt32(div(Asize[2], TS))
         for k in UInt32(1):UInt32(TS)
-            @inbounds acc += Asub[(k - 1)*TS + (row - 1 ) + 1] * Bsub[(col - 1) * TS + (k - 1) + 1]
+            @inbounds acc += Asub[(k - 1)+ (row - 1 ) + 1] * Asub[(row - 1) * TS + (k - 1) + 1]
         end
-        # Synchronise before loading the next tile
         synchronize_threads(state)
     end
-
-    # Store the final result in out
     @inbounds out[row] = acc
-
     return
-
 end
 
 
@@ -38,6 +23,7 @@ function matmul!(dest::GPUArray, a::GPUArray{T, 2}, b::GPUArray{T, 2}) where T
     Asize = UInt32.(Asize)
     Bsize = UInt32.(Bsize)
     config = ((div(Asize[1], TS), div(Bsize[2], TS)), (TS, TS))
+    print("div: $(div(Asize[2], TS))\n")
     gpu_call(matmul_kernel, dest, (a,b, dest, Asize, Bsize, outSize, Val{UInt32(32)}(), Val{UInt32(1024)}(), Val{1024}()), config)
     dest
 end
